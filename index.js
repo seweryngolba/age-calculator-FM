0a568b40e3319b4e6404aeca187931759879676c
Seweryn Golba <sgoolb@gmail.com>
2023-10-16 16:02:48 +0200

    js commit

--- index.js
+++ index.js
@@ -5,34 +5,40 @@ const yearInput = document.querySelector("#year");
 const resultYears = document.querySelector(".result .score:nth-child(1) h2");
 const resultMonths = document.querySelector(".result .score:nth-child(2) h2");
 const resultDays = document.querySelector(".result .score:nth-child(3) h2");
+const dayLabel = document.querySelector(".day-label");
+const monthLabel = document.querySelector(".month-label");
+const yearLabel = document.querySelector(".year-label");
 
 countButton.addEventListener("click", calculateAge);
 
 function calculateAge() {
   clearErrorStyles();
+
   const day = parseInt(dayInput.value);
   const month = parseInt(monthInput.value);
   const year = parseInt(yearInput.value);
 
-  let isValid = true;
+  let errorFields = [];
 
-  if (!isValidDate(day, month, year)) {
-    if (day < 1 || day > 31) {
-      showErrorStyles(dayInput);
-    }
-
-    if (month < 1 || month > 12) {
-      showErrorStyles(monthInput);
-    }
-
-    if (year < 1 || year > new Date().getFullYear()) {
-      showErrorStyles(yearInput);
-    }
-
-    isValid = false;
+  if (isNaN(day) || day < 1 || day > 31) {
+    showErrorStyles(dayInput);
+    showErrorLabel(dayLabel);
+    errorFields.push(dayLabel);
   }
 
-  if (!isValid) {
+  if (isNaN(month) || month < 1 || month > 12) {
+    showErrorStyles(monthInput);
+    showErrorLabel(monthLabel);
+    errorFields.push(monthLabel);
+  }
+
+  if (isNaN(year) || year < 1 || year > new Date().getFullYear()) {
+    showErrorStyles(yearInput);
+    showErrorLabel(yearLabel);
+    errorFields.push(yearLabel);
+  }
+
+  if (errorFields.length > 0) {
     return;
   }
 
@@ -41,46 +47,34 @@ function calculateAge() {
   const newDate = new Date(currentDate - birthDate);
 
   if (birthDate > currentDate) {
-    alert("Not in future");
+    alert("Not in the future");
     return;
   }
 
-  const years = newDate.getFullYear() - 1970;
-  const months = newDate.getMonth();
-  const days = newDate.getDate() - 1;
-
-  resultYears.textContent = years;
-  resultMonths.textContent = months;
-  resultDays.textContent = days;
+  resultYears.textContent = newDate.getFullYear() - 1970;
+  resultMonths.textContent = newDate.getMonth();
+  resultDays.textContent = newDate.getDate() - 1;
 }
 
 function clearErrorStyles() {
   dayInput.classList.remove("error");
   monthInput.classList.remove("error");
   yearInput.classList.remove("error");
-}
-
-function isValidDate(day, month, year) {
-  let valid = true;
-
-  if (day < 1 || day > 31) {
-    showErrorStyles(dayInput);
-    valid = false;
-  }
-
-  if (month < 1 || month > 12) {
-    showErrorStyles(monthInput);
-    valid = false;
-  }
-
-  if (year < 1 || year > new Date().getFullYear()) {
-    showErrorStyles(yearInput);
-    valid = false;
-  }
-
-  return valid;
+  clearErrorLabel(dayLabel);
+  clearErrorLabel(monthLabel);
+  clearErrorLabel(yearLabel);
 }
 
 function showErrorStyles(input) {
   input.classList.add("error");
+}
+
+function showErrorLabel(label) {
+  label.textContent = "Error Date";
+  label.classList.add("error-label");
+}
+
+function clearErrorLabel(label) {
+  label.textContent = label.getAttribute("for").toUpperCase();
+  label.classList.remove("error-label");
 }
